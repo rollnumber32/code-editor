af3358b51182f3f017e36d40e7cb5a7d9a22e5e8
Lakshay Mittal <20bca1209@cuchd.in>
2023-01-13 23:58:23 +0530

    Added local storage support

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import useLocalStorage from "./useLocalStorage";
+import { useEffect } from "react";
 import Editor from "./Editor";
 
 function App() {
-  const [srcDoc, setSrcDoc] = useState("");
-  const [HTML, setHTML] = useState(null);
-  const [CSS, setCSS] = useState(null);
-  const [JS, setJS] = useState(null);
+  const [srcDoc, setSrcDoc] = useLocalStorage("srcDoc", "");
+  const [HTML, setHTML] = useLocalStorage("HTML", null);
+  const [CSS, setCSS] = useLocalStorage("CSS", null);
+  const [JS, setJS] = useLocalStorage("JS", null);
 
   const handleHTMLChange = (editor, data, value) => {
     setHTML(value);
@@ -20,16 +21,28 @@ function App() {
     setJS(value);
   };
 
+  const clearData = () => {
+    setHTML("");
+    setCSS("");
+    setJS("");
+  };
+
   useEffect(() => {
     const timeout = setTimeout(() => {
       setSrcDoc(`${HTML} <style> ${CSS} </style> <script> ${JS} </script>`);
     }, 2500);
 
     return () => clearTimeout(timeout);
-  }, [HTML, CSS, JS]);
+  }, [HTML, CSS, JS, setSrcDoc]);
 
   return (
     <div>
+      <header>
+        <h1>Code Editor</h1>
+        <button className="refresh-btn" onClick={clearData}>
+          Reset Everything
+        </button>
+      </header>
       <div className="top-pane">
         <Editor type="HTML" value={HTML} handleChange={handleHTMLChange} />
         <Editor type="CSS" value={CSS} handleChange={handleCSSChange} />
